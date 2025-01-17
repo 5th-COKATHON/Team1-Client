230a11681dbc4ee9ce3c6264172ee9319fe9cf4a
Ki-Tak <lee08tain@gmail.com>
2025-01-17 21:21:41 +0900

    feat: 컴포넌트 파일 추가

---
+++ src/components/Trash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+export const Trash = () => {
+    return (
+        <DotLottieReact
+            src="https://lottie.host/e4d2cac3-6c3f-4248-aa07-1625ade6a4b0/Ydr3ImxFaJ.lottie"
+            loop
+            autoplay
+        />
+    );
+};
