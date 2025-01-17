f460c772f353dfa599d85f931c7ec34f95791634
AnSui22 <ansui218@ewhain.net>
2025-01-18 00:48:48 +0900

    feat: 로그인 페이지 구현 완료

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,45 +1,54 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/images/logo.png";
+import { apiLogin } from "../apis";
 
 const Login = () => {
   const navigation = useNavigate();
   const [email, setEmail] = useState("");
-
   const handleChange = (e) => {
     setEmail(e.target.value);
   };
 
-  const handleClick = () => {
-    // 백에 email post하고 이동
+  const handleClick = async () => {
+    const res = await apiLogin(email);
+    const memberId = res.data.id;
+    console.log(memberId);
+    localStorage.setItem(memberId);
     navigation("/home");
   };
 
   return (
-    <div className="flex items-center flex-col w-full h-full bg-[url('/src/assets/images/loginbg.png')] bg-cover">
+    <div className="flexcenter flex-col w-full gap-10 h-full bg-[url('/src/assets/images/loginbg.png')] bg-cover">
       <div className="flexcenter flex-col py-10 w-3/12">
         <img src={logo} />
       </div>
       <div className="flexcenter flex flex-col gap-4 w-3/12">
-        <div className="w-full font-SB">
+        <div className="flex flex-col w-full font-SB gap-2">
           <h2>아이디</h2>
           <input
-            className="logininput"
+            className="logininput border-4"
             placeholder="아이디를 입력해주세요"
             value={email}
             onChange={handleChange}
           />
         </div>
-        <div className="w-full">
+        <div className="flex flex-col w-full font-SB gap-2">
           <h2>비밀번호</h2>
-          <input className="logininput" placeholder="비밀번호를 입력해주세요" />
+          <input
+            className="logininput border-4"
+            placeholder="비밀번호를 입력해주세요"
+          />
         </div>
-        <button
-          onClick={handleClick}
-          className="logininput text-white bg-login-pink "
-        >
-          로그인하기
-        </button>
+        <div className="w-full py-4">
+          <button
+            disabled={!email}
+            onClick={handleClick}
+            className="logininput text-white bg-gray-400 enabled:bg-login-pink"
+          >
+            로그인하기
+          </button>
+        </div>
       </div>
     </div>
   );
